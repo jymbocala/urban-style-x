d3d7004a5f95fe0ba339f42040ef88d3b6aca21a
Jym Bocala <jym.bocala@gmail.com>
2023-06-12 01:40:22 +1000

    import themes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scenese/home/Home";
 
+// function that automatically scrolls the user to the top of the page once clicked
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+};
 
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
+  return <div className="app">
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
