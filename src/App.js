75715d93952fa290c72e408a50d8473a39304932
Jym Bocala <jym.bocala@gmail.com>
2023-06-13 02:24:50 +1000

    add Boxes and IconButtons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./scenes/home/Home";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
 
 // function that automatically scrolls the user to the top of the page once clicked
 const ScrollToTop = () => {
@@ -20,6 +21,7 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
