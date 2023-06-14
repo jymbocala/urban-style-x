a3aab602e697577b4b359299943536ff619240c1
Jym Bocala <jym.bocala@gmail.com>
2023-06-14 22:22:24 +1000

    add actions section for cart menu modal

--- src/scenes/global/CartMenu.jsx
+++ src/scenes/global/CartMenu.jsx
@@ -111,12 +111,11 @@ const CartMenu = () => {
                           <AddIcon />
                         </IconButton>
                       </Box>
+                      {/* PRICE */}
+                      <Typography fontWeight="bold">
+                        ${item.attributes.price}
+                      </Typography>
                     </FlexBox>
-
-                    {/* PRICE */}
-                    <Typography fontWeight="bold">
-                      ${item.attributes.price}
-                    </Typography>
                   </Box>
                 </FlexBox>
                 <Divider />
@@ -125,7 +124,28 @@ const CartMenu = () => {
           </Box>
 
           {/* ACTIONS */}
-          <Box></Box>
+          <Box m="20px 0">
+            <FlexBox m="2-px 0">
+              <Typography fontWeight="bold">SUBTOTAL</Typography>
+              <Typography fontWeight="bold">${totalPrice}</Typography>
+            </FlexBox>
+            <Button
+              sx={{
+                backgroundColor: shades.primary[400],
+                color: "white",
+                borderRadius: 0,
+                minWidth: "100%",
+                padding: "20px 40px",
+                m: "20px 0",
+              }}
+              onClick={() => {
+                navigate("/checkout");
+                dispatch(setIsCartOpen({}));
+              }}
+            >
+              CHECKOUT
+            </Button>
+          </Box>
         </Box>
       </Box>
     </Box>
